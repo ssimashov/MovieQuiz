fe153a8d561c627fa3425a0e3a3d922140de06a4
Sergey Simashov <simashov@gmail.com>
2024-06-13 20:40:17 +0300

    Add tests

--- Services/MoviesLoader.swift
+++ Services/MoviesLoader.swift
@@ -12,9 +12,10 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
-    
+    //MARK: - NetworkClient
     private let networkClient = NetworkClient()
     
+    //MARK: - URL
     private var mostPopularMoviesUrl: URL {
         guard let url = URL(string: "https://tv-api.com/en/API/MostPopularTVs/k_zcuw1ytf") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
